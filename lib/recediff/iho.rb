33618648c0326c8fe52f683d2d2874d73077c20d
yokenzan <31175068+yokenzan@users.noreply.github.com>
2022-04-02 12:03:20 +0900

    add comment to avoid lint error

--- lib/recediff/iho.rb
+++ lib/recediff/iho.rb
@@ -2,15 +2,17 @@
 
 module Recediff
   class Iho
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
     C_レコード識別情報 = 0
-    C_保険者番号 = 1
+    C_保険者番号       = 1
     C_記号             = 2
     C_番号             = 3
-    C_診療実日数 = 4
-    C_合計点数 = 5
-    C_食事療養回数 = 7
+    C_診療実日数       = 4
+    C_合計点数         = 5
+    C_食事療養回数     = 7
     C_食事療養合計金額 = 8
     C_負担金額医療保険 = 11
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     def initialize(row)
       @row = row
--- lib/recediff/kohi.rb
+++ lib/recediff/kohi.rb
@@ -2,17 +2,19 @@
 
 module Recediff
   class Kohi
-    C_レコード識別情報 = 0
-    C_公費負担者番号 = 1
-    C_受給者番号 = 2
-    C_任意給付区分 = 3
-    C_診療実日数 = 4
-    C_合計点数 = 5
-    C_公費負担金額 = 6
+    # rubocop:disable Layout/ExtraSpacing, Layout/SpaceAroundOperators
+    C_レコード識別情報           = 0
+    C_公費負担者番号             = 1
+    C_受給者番号                 = 2
+    C_任意給付区分               = 3
+    C_診療実日数                 = 4
+    C_合計点数                   = 5
+    C_公費負担金額               = 6
     C_公費給付対象外来一部負担金 = 7
     C_公費給付対象入院一部負担金 = 8
-    C_食事療養回数 = 10
-    C_食事療養合計金額 = 11
+    C_食事療養回数               = 10
+    C_食事療養合計金額           = 11
+    # rubocop:enable Layout/ExtraSpacing, Layout/SpaceAroundOperators
 
     def initialize(row)
       @row = row
