4a6fcff7d4c81cae0d7ddf142bd530cb35c5d95e
yokenzan <31175068+yokenzan@users.noreply.github.com>
2022-12-16 00:59:07 +0900

    レセ電コードでマスタ項目が見つけられなかった場合のエラーレベルをERRORからWARNに変更

--- lib/receiptisan/model/receipt_computer/digitalized_receipt/parser/context.rb
+++ lib/receiptisan/model/receipt_computer/digitalized_receipt/parser/context.rb
@@ -44,15 +44,17 @@ module Receiptisan
             module ErrorContextReportable
               # @param e [Exception]
               # @return [void]
-              def report_error(e)
+              def report_error(e, severity = Logger::WARN)
                 # ブロックをつかっていないのはスタックトレースも表示するため
-                logger.error 'Exception occurred while parsing %s:%d:%s' % [
+                message = 'Exception occurred while parsing %s:%d:%s' % [
                   context.uke_file_path,
                   context.current_line_number,
                   context.current_line,
                 ]
-                logger.error 'RECEIPT ID:%d' % context.current_receipt_id if context.current_receipt_id
-                logger.error e
+                logger.add(severity, message)
+                # レセプト内にいるとき(IR行や最初のRE行の読込時以外)はレセプト番号も出力する
+                logger.add(severity, 'RECEIPT ID:%d' % context.current_receipt_id) if context.current_receipt_id
+                logger.add(severity, e)
               end
 
               def logger
